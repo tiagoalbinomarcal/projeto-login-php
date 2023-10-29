07483d5baac734e3cd8c48eaa3b0d7641e3709e6
Tiago Albino Marçal <136192132+tiagoalbinomarcal@users.noreply.github.com>
2023-10-29 20:12:51 -0300

    trying to online database

--- api/conexao.php
+++ api/conexao.php
@@ -1,9 +1,9 @@
 <?php
     
-    $username = 'root';
-    $password = '';
+    $username = 'sql10657725';
+    $password = 'VGihupUgNB';
     try{
-        $conn = new PDO('mysql:host=localhost;dbname=projetologin', $username, $password);
+        $conn = new PDO('mysql:host=sql10.freesqldatabase.com;dbname=sql10657725', $username, $password);
     }catch(Exeption $e){
         die('falha ao conectar com o banco de dados!');
     }
