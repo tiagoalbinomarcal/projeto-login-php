59a6dc2dcaa47cba732f8721e09c6ab42982b172
Tiago Albino Marçal <136192132+tiagoalbinomarcal@users.noreply.github.com>
2023-10-30 14:19:47 -0300

    var_dump $_SESSION 2

--- api/index.php
+++ api/index.php
@@ -26,7 +26,7 @@
             $_SESSION['nome'] = $usuario['nome'];
             $_SESSION['id'] = $usuario['id'];
             
-            header('location: api/pag.php');
+            //header('location: api/pag.php');
         }
 
         
