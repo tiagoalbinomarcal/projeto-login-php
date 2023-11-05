bc547f5c226fe34a4c1c1697df9dccc671b9c079
Tiago Albino Marçal <136192132+tiagoalbinomarcal@users.noreply.github.com>
2023-11-05 18:26:57 -0300

    connected with database

--- api/conexao.php
+++ api/conexao.php
@@ -1,11 +1,19 @@
 <?php
     
-    $username = 'sql10657725';
-    $password = 'VGihupUgNB';
+    $username = 'oonmaruvkhd828pvfixe';
+    $password = 'pscale_pw_wEGPvo2v8NAjhWY34zVqpU7MDnndBMtqv4erUhDtCI9';
+    $options = array(
+        PDO::MYSQL_ATTR_SSL_CA => 'C:\xampp\htdocs\php-projeto-login\projeto-login-php\api\cacert.pem',
+    );
+
+    
     try{
-        $conn = new PDO('mysql:host=sql10.freesqldatabase.com;dbname=sql10657725', $username, $password);
+        $conn = new PDO('mysql:host=aws.connect.psdb.cloud;dbname=php-project', $username, $password, $options);
     }catch(Exeption $e){
         die('falha ao conectar com o banco de dados!');
     }
    
+    //SSL -> sistema de decodificação
+    //CA file -> arquivo dos certificados autenticadores
+
 ?>
