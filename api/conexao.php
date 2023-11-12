767e39d1f4146dc3a06ea045b15719e510f0a8ec
Tiago Albino Marçal <136192132+tiagoalbinomarcal@users.noreply.github.com>
2023-11-12 19:46:32 -0300

    vercerl x planet scape (try 1)

--- api/conexao.php
+++ api/conexao.php
@@ -1,19 +1,4 @@
 <?php
-    
-    $username = 'j5m76eng7bqdl8p2fy6m';
-    $password = 'pscale_pw_mcEtqBDfU9bZ50WCY2E04DSHCd0Z2AcvCByOpttqUsC';
-    $options = array(
-        PDO::MYSQL_ATTR_SSL_CA => 'C:\xampp\htdocs\php-projeto-login\projeto-login-php\api\cacert2.pem',
-    );
-
-    
-    try{
-        $conn = new PDO('mysql:host=aws.connect.psdb.cloud;dbname=php-project', $username, $password, $options);
-    }catch(Exeption $e){
-        die('falha ao conectar com o banco de dados!');
-    }
-   
-    //SSL -> sistema de decodificação
-    //CA file -> arquivo dos certificados autenticadores
-
+  $dsn = "mysql:host={$_ENV["DB_HOST"]};dbname={$_ENV["DB_NAME"]}";
+  $pdo = new PDO($dsn, $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"]);
 ?>
