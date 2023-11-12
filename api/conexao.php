8c401475630e916396a3c9e4466af2cf48b71683
Tiago Albino Marçal <136192132+tiagoalbinomarcal@users.noreply.github.com>
2023-11-12 19:58:03 -0300

    vercerl x planet scape (try 2.3)
    -> try/catch insert
    -> $conn fix

--- api/conexao.php
+++ api/conexao.php
@@ -3,6 +3,10 @@
   $options = array(
     PDO::MYSQL_ATTR_SSL_CA => $_ENV["PLANETSCALE_SSL_CERT_PATH"],
   );
-
-  $pdo = new PDO($dsn, $_ENV["PLANETSCALE_DB_USERNAME"], $_ENV["PLANETSCALE_DB_PASSWORD"], $options);
+  try {
+    $conn = new PDO($dsn, $_ENV["PLANETSCALE_DB_USERNAME"], $_ENV["PLANETSCALE_DB_PASSWORD"], $options);
+    } catch (Exception $e) {
+    die($e);
+    }
+  
 ?>
